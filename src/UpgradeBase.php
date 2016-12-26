662388521f5744c28768f18e39921bcb059d7ae4
lilwil <lilwil@163.com>
2016-12-27 07:51:11 +0800

    增加插件升级基类

---
+++ src/UpgradeBase.php
@@ -0,0 +1,24 @@
+<?php
+namespace think;
+
+abstract class UpgradeBase
+{
+    
+    // 错误信息
+    protected $error;
+    
+    // 实现升级代码
+    abstract public function upgrade();
+    // 实现升级异常的回滚代码
+    abstract public function rollback();
+
+    /**
+     * 返回错误信息
+     * 
+     * @return type
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+}
