89b28039fc9f3ff178ce928d60f2cff7a90a1408
lilwil <lilwil@163.com>
2017-01-10 16:43:08 +0800

    改进钩子初始化

--- src/addon/AppInit.php
+++ src/addon/AppInit.php
@@ -8,17 +8,20 @@ use think\Hook;
 // 初始化钩子信息
 class AppInit
 {
+
     public function run()
     {
         // 获取系统配置
-        $data = App::$debug ? [] : Cache::get('hooks');
+        $data = (App::$debug || ! Cache::has('addons_hooks')) ? [] : Cache::get('addons_hooks');
         if (empty($data)) {
             $addons = [];
             $hooks = Db::name('hooks')->order('id')->column('addons', 'name');
             foreach ($hooks as $key => $value) {
                 if ($value) {
                     $names = explode(',', $value);
-                    $data = Db::name('addons')->where('status', 1)->where('name', 'in', $names)->column('name', 'id');
+                    $data = Db::name('addons')->where('status', 1)
+                        ->where('name', 'in', $names)
+                        ->column('name', 'id');
                     if ($data) {
                         $addon = array_intersect($names, $data);
                         $addons[$key] = array_map('get_addon_class', $addon);
@@ -26,10 +29,8 @@ class AppInit
                     }
                 }
             }
-            if (empty($addons)) {
-                Cache::set('hooks', $addons);
-            }
-        } else{
+            ! App::$debug && ! empty($addons) && Cache::set('addons_hooks', $addons);
+        } else {
             Hook::import($data, false);
         }
     }
